0a457cdc1b54dde6ed0a92128cbe32655f017d9a
tylermcgraw <=>
2024-09-21 07:51:07 -0400

    Added repeated calculations, json file for messages, and translation to Spanish

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,4 +1,69 @@
 const readline = require('readline-sync');
+const messages = require('./calculator_messages.json');
+
+let calculate = true;
+prompt(messages.language);
+let lang = readline.question();
+while (!['en', 'es'].includes(lang)) {
+  prompt(messages.invalidLanguage);
+  lang = readline.question();
+}
+console.log(lang);
+prompt(messages[lang].welcome);
+
+do {
+  prompt(messages[lang].num1);
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt(messages[lang].invalidNum);
+    number1 = readline.question();
+  }
+
+  prompt(messages[lang].num2);
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt(messages[lang].invalidNum);
+    number2 = readline.question();
+  }
+
+  prompt(messages[lang].operation);
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(messages[lang].invalidOp);
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`${messages[lang].result} ${output}`);
+
+  prompt(messages[lang].repeat);
+  let repeat = readline.question();
+  while (!['y', 'n'].includes(repeat)) {
+    prompt(messages[lang].invalidRepeat);
+    repeat = readline.question();
+  }
+  if (repeat === 'n') {
+    calculate = false;
+  }
+} while (calculate);
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -7,47 +72,3 @@ function prompt(message) {
 function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
-
-prompt('Welcome to Calculator!');
-
-prompt("What's the first number?");
-let number1 = readline.question();
-
-while (invalidNumber(number1)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number1 = readline.question();
-}
-
-prompt("What's the second number?");
-let number2 = readline.question();
-
-while (invalidNumber(number2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
-}
-
-prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3 or 4');
-  operation = readline.question();
-}
-
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-prompt(`The result is: ${output}`);
