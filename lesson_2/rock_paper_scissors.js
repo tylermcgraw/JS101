622d805961f9633fc6c2e0fa2267e579aa2ea3ed
tylermcgraw <=>
2024-09-24 15:19:12 -0400

    added best of 3 and cleaned up rock paper scissors lizard spock

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -7,16 +7,32 @@ const WIN_CONDITIONS = {
   lizard: ['paper', 'spock'],
   spock: ['rock', 'scissors']
 };
+const WIN_LIMIT = 3;
+let playerWins = 0;
+let computerWins = 0;
+let gameOver = false;
 
 function displayWinner(choice, computerChoice) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
   if (WIN_CONDITIONS[choice].includes(computerChoice)) {
     prompt('You win!');
+    playerWins++;
   } else if (WIN_CONDITIONS[computerChoice].includes(choice)) {
     prompt('Computer wins!');
+    computerWins++;
   } else {
     prompt("It's a tie!");
+  }
+}
+
+function displayGrandWinner() {
+  if (playerWins === WIN_LIMIT) {
+    prompt("Congratulations! You are the grand champion!");
+  } else if (computerWins === WIN_LIMIT) {
+    prompt("Sorry. The computer is the grand champion!");
+  } else {
+    prompt(`The current score is: Player: ${playerWins} Computer: ${computerWins}`);
   }
 }
 
@@ -33,26 +49,37 @@ function convertChoice(input) {
   return input;
 }
 
-while (true) {
+function resetGame() {
+  gameOver = false;
+  playerWins = 0;
+  computerWins = 0;
+}
+
+prompt(`Welcome to Rock, Paper, Scissors, Lizard, Spock! First to ${WIN_LIMIT} wins.`);
+while (!gameOver) {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   let choice = convertChoice(readline.question());
 
   while (!VALID_CHOICES.includes(choice)) {
     prompt("That's not a valid choice");
-    choice = readline.question();
+    choice = convertChoice(readline.question());
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
   displayWinner(choice, computerChoice);
+  displayGrandWinner();
+  gameOver = (playerWins === WIN_LIMIT || computerWins === WIN_LIMIT);
 
-  prompt('Do you want to play again (y/n)?');
-  let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
+  if (gameOver) {
+    prompt('Do you want to play again (y/n)?');
+    let answer = readline.question().toLowerCase();
+    while (answer[0] !== 'n' && answer[0] !== 'y') {
+      prompt('Please enter "y" or "n".');
+      answer = readline.question().toLowerCase();
+    }
+
+    if (answer[0] === 'y') resetGame();
   }
-
-  if (answer[0] !== 'y') break;
 }
