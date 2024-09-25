6f12c43bfdf372d6f2078c59eeb5575964afff01
tylermcgraw <=>
2024-09-25 09:39:59 -0400

    added FINAL_OUTCOME for messages and added comments

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -11,27 +11,40 @@ const WIN_LIMIT = 3;
 let playerWins = 0;
 let computerWins = 0;
 let gameOver = false;
+const GAME_OUTCOME = {
+  playerWin: 'You win!',
+  computerWin: 'Computer wins!',
+  tie: "It's a tie"
+};
+const FINAL_OUTCOME = {
+  playerChampion: 'Congratulations! You are the grand champion!',
+  computerChampion: 'Sorry. The computer is the grand champion!'
+};
 
-function displayWinner(choice, computerChoice) {
+// Is it ok that displayGameWinner does 2 things - displays the winner
+// and increments playerWins/computerWins?
+function displayGameWinner(choice, computerChoice) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
   if (WIN_CONDITIONS[choice].includes(computerChoice)) {
-    prompt('You win!');
     playerWins++;
+    prompt(GAME_OUTCOME.playerWin);
   } else if (WIN_CONDITIONS[computerChoice].includes(choice)) {
-    prompt('Computer wins!');
     computerWins++;
+    prompt(GAME_OUTCOME.computerWin);
   } else {
-    prompt("It's a tie!");
+    prompt(GAME_OUTCOME.tie);
   }
 }
 
 function displayGrandWinner() {
   if (playerWins === WIN_LIMIT) {
-    prompt("Congratulations! You are the grand champion!");
+    prompt(FINAL_OUTCOME.playerChampion);
   } else if (computerWins === WIN_LIMIT) {
-    prompt("Sorry. The computer is the grand champion!");
+    prompt(FINAL_OUTCOME.computerChampion);
   } else {
+    // I initially had this template literal in my FINAL_OUTCOME object, but I
+    // took it out because playerWins and computerWins need to be updated
     prompt(`The current score is: Player: ${playerWins} Computer: ${computerWins}`);
   }
 }
@@ -40,6 +53,8 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+// Converts substrings  of choices to valid choices
+// Ex. 'r' -> 'rock', 'sc' -> 'scissors'
 function convertChoice(input) {
   VALID_CHOICES.forEach((choice) => {
     if (input === choice.substring(0, input.length)) {
@@ -68,8 +83,9 @@ while (!gameOver) {
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  displayWinner(choice, computerChoice);
+  displayGameWinner(choice, computerChoice);
   displayGrandWinner();
+  prompt(playerWins);
   gameOver = (playerWins === WIN_LIMIT || computerWins === WIN_LIMIT);
 
   if (gameOver) {
